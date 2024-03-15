95854e754ad3ffb63ca960a3a05e371a13e73087
Ravi Teja Gudapati <tejainece@gmail.com>
2024-03-15 17:11:07 +0100

    Conv2D fixes

--- tensorcuda/src/nn2d/conv2d.cu
+++ tensorcuda/src/nn2d/conv2d.cu
@@ -24,10 +24,8 @@ __global__ void conv2dKernel(T* output, T* input, T* kernel, uint32_t groups, Di
   uint32_t groupLen = inpS.ch / groups;
   uint32_t batchId = outId / outS.ch;
   input += batchId * inpS.ch * inpS.r * inpS.c;
-  // uint32_t firstInpChannelId = (outChannel * inpS.ch)/outS.ch;
-  uint32_t firstInpChannelId = (outChannel * groupLen)/outS.ch;
+  uint32_t firstInpChannelId = (outChannel/groupLen) * groupLen;
   input += firstInpChannelId * inpS.c * inpS.r;
-  printf("outChannel: %d, groupLen: %d, firstInpChannelId: %d\n", outChannel, groupLen, firstInpChannelId);
 
   if (outR < outS.r && outC < outS.c) {
     T value = 0;
