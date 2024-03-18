ef300daca19f1af1f250cd72545cdf647358dd15
Ravi Teja Gudapati <tejainece@gmail.com>
2024-03-18 03:27:40 +0100

    Transpose2D bug fixes

--- tensorcuda/src/matrix/tranpose.cu
+++ tensorcuda/src/matrix/tranpose.cu
@@ -17,14 +17,18 @@ __global__ void transpose2d(T *out, const T *inp, Dim2 inSize) {
   inp += batch * inSize.r * inSize.c;
   out += batch * inSize.r * inSize.c;
 
+  Dim2 inpIdx{blockIdx.x * TILE_SIZE + threadIdx.y, blockIdx.y * TILE_SIZE + threadIdx.x};
+
   // TILE_SIZE+1 to avoid shared memory bank conflicts
   __shared__ T tile[TILE_SIZE][TILE_SIZE + 1];
 
   // coalesced read from global mem to shared mem row-by-row
   for (int rowOffset = 0; rowOffset < TILE_SIZE; rowOffset += BLOCK_ROWS) {
-    uint32_t row = outRow + rowOffset;
-    T val = inp[outCol * inSize.c + row];
-    tile[threadIdx.y + rowOffset][threadIdx.x] = val;
+    uint32_t row = inpIdx.r + rowOffset;
+    if (row < inSize.r && inpIdx.c < inSize.c) {
+      T val = inp[row * inSize.c + inpIdx.c];
+      tile[threadIdx.y + rowOffset][threadIdx.x] = val;
+    }
   }
 
   __syncthreads();
@@ -35,7 +39,8 @@ __global__ void transpose2d(T *out, const T *inp, Dim2 inSize) {
   for (int rowOffset = 0; rowOffset < TILE_SIZE; rowOffset += BLOCK_ROWS) {
     uint32_t row = outRow + rowOffset;
     if (row < outSize.r && outCol < outSize.c) {
-      out[row * outSize.c + outCol] = tile[threadIdx.y + rowOffset][threadIdx.x];
+      out[row * outSize.c + outCol] =
+          tile[threadIdx.x][threadIdx.y + rowOffset];
     }
   }
 }
@@ -58,8 +63,9 @@ const char *libtcCudaTranspose2d(
   config.gridDim.y = (outSize.r + TILE_SIZE - 1) / TILE_SIZE;
   config.gridDim.z = inSize.ch;
 
-  err =
-      cudaLaunchKernelEx(&config, transpose2d<double>, out, inp, inSize.toDim2());
+  err = cudaLaunchKernelEx(
+      &config, transpose2d<double>, out, inp, inSize.toDim2()
+  );
   if (err != cudaSuccess) {
     return cudaGetErrorString(err);
   }
