a58b87cbc35d6683456b8de13419e0bb3bd96c29
Ravi Teja Gudapati <tejainece@gmail.com>
2024-03-21 17:51:33 +0100

    WIP! num data types

--- tensorcuda/src/vector.cu
+++ tensorcuda/src/vector.cu
@@ -7,8 +7,8 @@
 
 // TODO implement stride and split
 /// Adds two tensors
-template<typename T>
-__global__ void add2(T* out, const T* in1, const T* in2, uint64_t n) {
+template<typename O, I1, I2>
+__global__ void add2(O* out, const I1* in1, const I2* in2, uint64_t n) {
   uint32_t numThreads = blockDim.x * gridDim.x;
   uint32_t thId = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -17,7 +17,7 @@ __global__ void add2(T* out, const T* in1, const T* in2, uint64_t n) {
   }
 }
 
-const char* libtcCudaAdd2(libtcCudaStream& stream, double* out, const double* in1, const double* in2, uint64_t n) {
+const char* libtcCudaAdd2(libtcCudaStream& stream, void* out, const void* in1, const void* in2, uint64_t n, dtype oType, dtype i1Type, dtype i2Type) {
   auto err = cudaSetDevice(stream.device);
   if (err != cudaSuccess) {
     return cudaGetErrorString(err);
@@ -43,7 +43,21 @@ const char* libtcCudaAdd2(libtcCudaStream& stream, double* out, const double* in
     config.gridDim.x = (n + props.maxThreadsPerBlock - 1) / props.maxThreadsPerBlock;
   }
 
-  err = cudaLaunchKernelEx(&config, add2<double>, out, in1, in2, n);
+  if(oType == dtype::f64 && i1Type == dtype::f64 && i2Type == dtype::f64) {
+    err = cudaLaunchKernelEx(&config, add2<double, double, double>, out, in1, in2, n);
+  } else if(oType == dtype::f32 && i1Type == dtype::f32 && i2Type == dtype::f32) {
+    err = cudaLaunchKernelEx(&config, add2<float, float, float>, out, in1, in2, n);
+  } else if(oType == dtype::f32 && i1Type == dtype::f32 && i2Type == dtype::f64) {
+    err = cudaLaunchKernelEx(&config, add2<float, float, double>, out, in1, in2, n);
+  } else if(oType == dtype::f64 && i1Type == dtype::f64 && i2Type == dtype::f32) {
+    err = cudaLaunchKernelEx(&config, add2<double, double, float>, out, in1, in2, n);
+  } else if(oType == dtype::f32 && i1Type == dtype::f64 && i2Type == dtype::f32) {
+    err = cudaLaunchKernelEx(&config, add2<float, double, float>, out, in1, in2, n);
+  } else if(oType == dtype::f64 && i1Type == dtype::f32 && i2Type == dtype::f64) {
+    err = cudaLaunchKernelEx(&config, add2<double, float, double>, out, in1, in2, n);
+  } else {
+    return "Unsupported data type";
+  }
   if (err != cudaSuccess) {
     return cudaGetErrorString(err);
   }
