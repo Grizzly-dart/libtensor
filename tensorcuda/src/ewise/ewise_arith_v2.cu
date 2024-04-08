fb5c6c89763e07f29f34c577b0376284deb0e4f9
Ravi Teja Gudapati <tejainece@gmail.com>
2024-04-08 09:56:38 +0200

    WIP CLion

---
+++ tensorcuda/src/ewise/ewise_arith_v2.cu
@@ -0,0 +1,23 @@
+#include <cstdint>
+#include <string>
+
+#include <cuda_runtime.h>
+
+#include <tensorcuda.hpp>
+
+/// Adds two tensors
+template <typename O, typename I1, typename I2>
+__global__ void plus(
+    O *out, I1 *inp1, I2 * inp2, I2 scalar, uint64_t n, uint8_t flipScalar
+) {
+  uint32_t numThreads = blockDim.x * gridDim.x;
+  uint32_t thId = threadIdx.x + blockIdx.x * blockDim.x;
+
+  for (uint64_t i = thId; i < n; i += numThreads) {
+    if (inp2 == nullptr) {
+      out[i] = inp1[i] + scalar;
+    } else {
+      out[i] = inp1[i] + inp2[i];
+    }
+  }
+}
