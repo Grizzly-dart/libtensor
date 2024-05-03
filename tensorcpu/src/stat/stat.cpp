23571990ff49662c2ed384d0390c5a1dcadc71ad
Ravi Teja Gudapati <tejainece@gmail.com>
2024-05-03 19:41:15 +0200

    WIP tcMean

--- tensorcpu/src/stat/stat.cpp
+++ tensorcpu/src/stat/stat.cpp
@@ -9,6 +9,37 @@
 #include "tensorcpu.hpp"
 #include "typed_array.hpp"
 
+template <typename O, typename I> void mean_parallel(O *out, I *inp, uint64_t nel) {
+  uint64_t laneSize = MeanSimd<O, I>::sizeSimd;
+  using ISimd = typename MeanSimd<O, I>::ISimdType;
+
+  std::function<MeanSimd<O, I>(uint64_t, uint64_t)> kernel =
+      [laneSize, inp](uint64_t start, uint64_t end) {
+        MeanSimd<O, I> ret;
+        ISimd i1;
+        for (uint64_t lane = start; lane < end; lane += laneSize) {
+          memcpy(&i1, inp + lane, laneSize * sizeof(I));
+          ret.consumeSimd(i1);
+        }
+        return ret;
+      };
+  MeanSimd<O, I> foldedMean;
+  /* TODO
+  parallelSimdFold<MeanSimd<O, I>>(
+      nel, laneSize, kernel, foldedMean,
+      [](MeanSimd<O, I> &a, MeanSimd<O, I> b) { a.merge(b); }
+  );
+   */
+
+  Mean<O, I> mean = foldedMean.materialize();
+  uint64_t tail = nel % laneSize;
+  for (uint64_t i = nel - tail; i < nel; i++) {
+    mean.consume(inp[i]);
+  }
+
+  *out = mean.mean;
+}
+
 template <typename O, typename I> void tcMean(O *out, I *inp, uint64_t nel) {
   uint64_t laneSize = MeanSimd<O, I>::sizeSimd;
   using ISimd = typename MeanSimd<O, I>::ISimdType;
